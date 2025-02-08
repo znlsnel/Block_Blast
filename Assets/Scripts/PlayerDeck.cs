4b2447d3f4eeff5c2a61bd5792a76ce00830341d
Udi <dnel0605@gmail.com>
2025-02-08 15:25:34 +0900

    [feat] block auto setting
    댁에 블록이 자동으로 세팅 되도록 설계

--- Assets/Scripts/PlayerDeck.cs
+++ Assets/Scripts/PlayerDeck.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class PlayerDeck : MonoBehaviour
 {
-	float width = 5.0f;
+	[SerializeField] int blockCount = 3;
+	[SerializeField] List<GameObject> blocks= new List<GameObject>();
 
-	List<float> points = new List<float>();
+	float width = 3.8f;
+
+	List<Vector3> points = new List<Vector3>();
+
+
 
 	private void Awake()
 	{
-		SpriteRenderer rdr = GetComponent<SpriteRenderer>();
-		if (rdr != null && rdr.sprite != null)
-			width = rdr.sprite.bounds.size.x;
+		float offset = width / (blockCount + 1);
+		Vector3 pos = transform.position + new Vector3(-width/2, 0, 0);
+
+		for (int i = 0; i < blockCount; i++)
+		{
+			pos += new Vector3(offset, 0, 0); 
+			points.Add(pos); 
+		}
+
+	}
+
+	private void Start()
+	{
+		SpawnBlock();
+	} 
+
+
+	void SpawnBlock()
+	{
+		foreach (Vector3 pos in points)
+		{
+			var go = Instantiate(blocks[Random.Range(0, blocks.Count)]);
+			go.transform.position = pos;
+			go.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+		}
 	}
 }
